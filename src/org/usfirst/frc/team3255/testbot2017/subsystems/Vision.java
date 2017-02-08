2d88467633ec26f043bb605c33e780fecde26908
Tayler Uva <tayleruva@gmail.com>
2017-02-07 16:33:43 -0800

    Moved declarations to outside thread
    Tested and working.
    Switching seems faster also.

--- src/org/usfirst/frc/team3255/testbot2017/subsystems/Vision.java
+++ src/org/usfirst/frc/team3255/testbot2017/subsystems/Vision.java
@@ -41,16 +41,16 @@ public class Vision extends Subsystem implements Runnable {
 	@Override
 	public void run() {
 		
-		UsbCamera camera1 = CameraServer.getInstance().startAutomaticCapture(0);
-        camera1.setResolution(320, 240);
-        camera1.setFPS(30);
-        UsbCamera camera2 = CameraServer.getInstance().startAutomaticCapture(1);
-        camera2.setResolution(320, 240);
-        camera2.setFPS(30);
+		frontCamera = CameraServer.getInstance().startAutomaticCapture(0);
+        frontCamera.setResolution(320, 240);
+        frontCamera.setFPS(30);
+        rearCamera = CameraServer.getInstance().startAutomaticCapture(1);
+        rearCamera.setResolution(320, 240);
+        rearCamera.setFPS(30);
         
-        CvSink cvSink1 = CameraServer.getInstance().getVideo(camera1);
-        CvSink cvSink2 = CameraServer.getInstance().getVideo(camera2);
-        CvSource outputStream = CameraServer.getInstance().putVideo("Switcher", 320, 240);
+        frontSink = CameraServer.getInstance().getVideo(frontCamera);
+        rearSink = CameraServer.getInstance().getVideo(rearCamera);
+        outputStream = CameraServer.getInstance().putVideo("Switcher", 320, 240);
         
         Mat image = new Mat();
 
@@ -76,13 +76,13 @@ public class Vision extends Subsystem implements Runnable {
 			 */
 			
 			if(frontCamOn){
-                cvSink2.setEnabled(false);
-                cvSink1.setEnabled(true);
-                cvSink1.grabFrame(image);
+                rearSink.setEnabled(false);
+                frontSink.setEnabled(true);
+                frontSink.grabFrame(image);
               } else{
-                cvSink1.setEnabled(false);
-                cvSink2.setEnabled(true);
-                cvSink2.grabFrame(image);     
+                frontSink.setEnabled(false);
+                rearSink.setEnabled(true);
+                rearSink.grabFrame(image);     
               }
 			
 			outputStream.putFrame(image);
