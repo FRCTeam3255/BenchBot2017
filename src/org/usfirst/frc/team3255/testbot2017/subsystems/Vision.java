f0ef0d42d8a4d20153bcbc570216653fd9d6fa79
Tayler Uva <tayleruva@gmail.com>
2017-02-08 12:16:26 -0800

    Cleaned up warning and added detailed comments
    Comments explain what the code is doing for teaching purposes.

--- src/org/usfirst/frc/team3255/testbot2017/subsystems/Vision.java
+++ src/org/usfirst/frc/team3255/testbot2017/subsystems/Vision.java
@@ -1,16 +1,9 @@
 package org.usfirst.frc.team3255.testbot2017.subsystems;
 
 import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Scalar;
-import org.opencv.imgproc.Imgproc;
-import org.usfirst.frc.team3255.testbot2017.OI;
-
 import edu.wpi.cscore.CvSink;
 import edu.wpi.cscore.CvSource;
 import edu.wpi.cscore.UsbCamera;
-import edu.wpi.cscore.VideoMode;
-import edu.wpi.cscore.VideoSink;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -20,8 +13,6 @@ public class Vision extends Subsystem implements Runnable {
 	private UsbCamera rearCamera = null;
 	
 	private CameraServer cameraServer = null;
-	
-	private VideoSink usbCameraSink = null;
 	
 	private CvSink frontSink = null;
 	private CvSink rearSink = null;
@@ -40,24 +31,32 @@ public class Vision extends Subsystem implements Runnable {
 	
 	@Override
 	public void run() {
+		
+		// ===== Initializations ===== //
+		//Creates CameraServer
 		cameraServer = CameraServer.getInstance();
-		
+		//Starts capture of the frontCamera
 		frontCamera = cameraServer.startAutomaticCapture(0);
-		
+		//Sets resolution and frame rate of frontCamera
         frontCamera.setResolution(320, 240);
         frontCamera.setFPS(30);
-        
-        	rearCamera = cameraServer.startAutomaticCapture(1);
-		
+        //Starts capture of the rearCamera
+        rearCamera = cameraServer.startAutomaticCapture(1);
+		//Sets resolution and frame rate of rearCamera
         rearCamera.setResolution(320, 240);
         rearCamera.setFPS(30);
         
+        //Creates sinks (steams) and have them get video from their respective cameras
         frontSink = cameraServer.getVideo(frontCamera);
         rearSink = cameraServer.getVideo(rearCamera);
+        //Creates outPutStream called "Switcher" which will be put on the dashboard
         outputStream = cameraServer.putVideo("Switcher", 320, 240);
         
+        //Creates a mat to hold image
         Mat image = new Mat();
 
+        
+        				////======= Thread Loop ======////
 		// This cannot be 'true'. The program will never exit if it is. This
 		// lets the robot stop this thread when restarting robot code or
 		// deploying.
@@ -72,52 +71,51 @@ public class Vision extends Subsystem implements Runnable {
 				// skip the rest of the current iteration
 				continue;
 			}
+			*/
 			
+			/*
+			// ===== Drawing on image ===== //
 			// Put a rectangle on the image
-			Imgproc.rectangle(mat, new Point(100, 100), new Point(400, 400),
+			Imgproc.rectangle(image, new Point(100, 100), new Point(400, 400),
 					new Scalar(255, 255, 255), 5);
-			// Give the output stream a new image to display
-			 */
+			*/
 			
+			// ===== Camera Selection ==== //
+			// Checks to see if boolean is set to true (changed in selecting methods)
+			/* To avoid bandwidth & stream errors, the current camera
+			FPS must be set to 0 and its sink must be disabled before
+			the new camera's sink is enabled and its FPS is set to 30. */
 			if(frontCamOn){
-				rearCamera.setFPS(0);
-                rearSink.setEnabled(false);
-                frontCamera.setFPS(30);
-                frontSink.setEnabled(true);
-                frontSink.grabFrame(image);
-              } else{
-            	frontCamera.setFPS(0);
-                frontSink.setEnabled(false);
-                rearCamera.setFPS(30);
-                rearSink.setEnabled(true);
-                rearSink.grabFrame(image);     
+				rearCamera.setFPS(0);		//Sets FPS to 0 for rear camera
+                rearSink.setEnabled(false); //Disables stream for rear camera 
+                frontCamera.setFPS(30);		//Sets FPS to 30 for front camera
+                frontSink.setEnabled(true); //Disables stream for rear camera
+                frontSink.grabFrame(image); //Grabs the frames from front camera
+              } 
+			else{
+            	frontCamera.setFPS(0);		//Sets FPS to 0 for front camera
+                frontSink.setEnabled(false);//Disables stream for front camera 
+                rearCamera.setFPS(30);		//Sets FPS to 30 for rear camera
+                rearSink.setEnabled(true);	//Disables stream for rear camera
+                rearSink.grabFrame(image);  //Grabs the frames from rear camera   
               }
 			
-			outputStream.putFrame(image);
+			// ===== Output Steam to Dashboard ===== //
+			outputStream.putFrame(image);	//Puts grabbed frames in output stream "Switcher"
 		}
 	}
 	
 	public void selectForwardCamera() {
+		//Sets frontCamOn boolean to true
 		frontCamOn = true;
-		
-		/*
-		usbCameraSink.setEnabled(false);
-		usbCameraSink.setSource(frontCamera);
-		usbCameraSink.setEnabled(true);
-		*/
 	}
 	
 	public void selectRearCamera() {
+		//Sets frontCamOn boolean to false
 		frontCamOn = false;
-		/*
-		usbCameraSink.setEnabled(false);
-		usbCameraSink.setSource(rearCamera);
-		usbCameraSink.setEnabled(true);
-		*/
 	}
 
 	@Override
 	protected void initDefaultCommand() {
-		// TODO Auto-generated method stub
 	}
 }
